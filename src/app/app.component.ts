dcc102506e6b9ae40e8c2b5d4e1b3a59ce2eda98
Silvia Sottile <silviasottile94@gmail.com>
2018-07-13 12:54:08 +0200

    sono sulla buona strada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,54 @@ import { Component } from '@angular/core';
 import { Http, Response, Headers } from '@angular/http';
 // import { map } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
+import { getDefaultService } from 'selenium-webdriver/opera';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SIoT';
+  title = 'SIoT Application';
   private endpoint = 'http://localhost:8080/Sim/SIoT/Server/';
-  data: any = {};
+  data: any = null;
   resultTitle = 'The results will be shown here...';
 
   constructor(private _http: Http) {
   }
 
-  // TODO: scrivere i metodi per prendere tutte le robe
-  // TODO: tra cui quello per effettuare la transazione.. Da aggiungere anche nel backend
-
   // Example:
 
-  public getAllLogs() {
-    return this._http.get(this.endpoint + 'COMPLETA API')
-                .pipe(map((res: Response) => res.json()))
-                 .subscribe(data => {
-                        this.data = data;
-                        this.resultTitle = 'All logs:';
-                        console.log(this.data);
+  public getSVER() {
+    const list: number[] = [];
+    const regex = new RegExp('\\d+');
+
+    return this._http.get(this.endpoint + '0/Clients') // http://localhost:8080/Sim/SIoT/Server/0/Clients
+                .toPromise().then((res) => {
+                  this.data = res.json();
+                  this.resultTitle = 'List of clients in SVER 0: ';
+                  console.log(this.data);
                 });
   }
+
+  public getSVE(_id: number) {
+    return this._http.get(this.endpoint + '0/SVER/' + _id)
+                .toPromise().then((res) => {
+                  // TODO: sistemare sta cosa, far comparire i dati
+                  // this.data = res.json();
+                  console.log(res.json());
+                });
+  }
+
+  public makeTransaction(_id: number, uid: number, amount: string, address: string) {
+    return this._http.get(this.endpoint + '0/SVER/' + _id)
+                .toPromise().then((res) => {
+                  // TODO: COMPLETARE
+                  // this.data = res.json();
+                  console.log(res.json());
+                });
+  }
+
 }
+
+
+
